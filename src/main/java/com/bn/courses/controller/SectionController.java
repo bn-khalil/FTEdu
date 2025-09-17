7617b951470a464a7c2eb7ffaec05144d88d7377
bn-khalil <benkhaliltouda3201@gmail.com>
2025-09-17 18:38:04 +0100

    implement fetch sections for each course

--- src/main/java/com/bn/courses/controller/SectionController.java
+++ src/main/java/com/bn/courses/controller/SectionController.java
@@ -2,23 +2,27 @@ package com.bn.courses.controller;
 
 import com.bn.courses.dto.SectionDTO;
 import com.bn.courses.repositories.SectionRepository;
+import com.bn.courses.service.SectionService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
 
 @RestController
-@RequestMapping("services")
+@RequestMapping("sections")
 public class SectionController {
 
-    private final SectionRepository sectionRepository;
+    private final SectionService sectionService;
 
-    public SectionController(SectionRepository sectionRepository) {
-        this.sectionRepository = sectionRepository;
+    public SectionController(SectionService sectionService) {
+        this.sectionService = sectionService;
     }
 
-    @GetMapping("/")
-    public ResponseEntity<SectionDTO> getAllSection() {
-        return ResponseEntity.ok(this.sectionRepository.find);
+    @GetMapping("/course/{course_id}")
+    public ResponseEntity<List<SectionDTO>> getSectionsWithCourse(@PathVariable Long course_id) {
+        return ResponseEntity.ok(
+                this.sectionService.findServicesWithCourseId(course_id)
+        );
     }
 }
--- src/main/java/com/bn/courses/serviceImpl/SectionServiceImpl.java
+++ src/main/java/com/bn/courses/serviceImpl/SectionServiceImpl.java
@@ -6,9 +6,11 @@ import com.bn.courses.mapper.SectionMapper;
 import com.bn.courses.model.Section;
 import com.bn.courses.repositories.SectionRepository;
 import com.bn.courses.service.SectionService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class SectionServiceImpl implements SectionService {
     private final SectionRepository sectionRepository;
     private final SectionMapper sectionMapper;
